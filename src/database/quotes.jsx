f44e8e79a6b041c7017ddf36bb2fa8f85cb5dacf
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-06-10 17:07:38 +0530

    .

--- src/database/quotes.jsx
+++ src/database/quotes.jsx
@@ -1,6 +1,6 @@
 export const quotes=[
     "one day you gonna run free?",
-    "pain && dynamics is your best friend ",
+    "dynamics is your best friend ",
     "feeling sad try running looking at sky backwards,",
     "i don't care if i die today or now",
     "it's nice to have a friend",
